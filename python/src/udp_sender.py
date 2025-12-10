1b6c11edc65e10d69272f1fcac95f327145f70e8
shigekuni000 <kimura0614i@gmail.com>
2025-12-10 16:23:23 +0900

    udp通信処理を追加。printで送信内容を確認できる。

--- python/src/udp_sender.py
+++ python/src/udp_sender.py
@@ -1,2 +1,22 @@
 # udp_sender.py
+import socket
+from typing import Tuple
+
+class UDPSender:
+	"""
+	UDP通信でデータを送信するラッパークラス
+	"""
+	def __init__(self, ip: str, port: int) -> None:
+		self.address: Tuple[str, int] = (ip, port)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	def send(self, message: str) -> None:
+		"""
+		指定したメッセージをUDPで送信
+		Args:
+			message (str): 送信するデータ
+		"""
+		self.sock.sendto(message.encode('utf-8'), self.address)
+		# fordebug
+		print(f"Sent UDP message to {self.address}: {message}")
 
